2d8ddf206774bfc59c01172a54817db85a9354ea
Yixiong <Christianding@me.com>
2021-09-17 17:09:30 +0800

    Update DRL.py

--- DRL.py
+++ DRL.py
@@ -13,14 +13,14 @@ from collections import deque
 # （比如我们选择向左推动小车并执行后，小车位置更加偏左，而杆的角度更加偏右，Gym 将新的角度和位置返回给我们。而如果杆在这一回合仍没有倒下，Gym
 # 同时返回给我们一个小的正奖励）。这个过程可以一直迭代下去，直到游戏终止（比如杆倒下了）。
 
-env = gym.make('CartPole-v1')       # 实例化一个游戏环境， 参数为游戏名称
-state = env.reset()                 # 初始化环境，获得初始状态
-while True:
-    env.render()                    # 对当前帧进行渲染，绘图到屏幕
-    action = model.predict(state)   # 假设有一个训练好的模型， 能够通过当前状态预测出这时应该进行的动作
-    next_state, reward, done, info = env.step(action)  # 让环境执行动作，获得执行动作的下一个状态，动作的状态，游戏是否结束以及额外信息
-    if done:                        # 游戏结束则退出循环
-        break
+# env = gym.make('CartPole-v1')       # 实例化一个游戏环境， 参数为游戏名称
+# state = env.reset()                 # 初始化环境，获得初始状态
+# while True:
+#     env.render()                    # 对当前帧进行渲染，绘图到屏幕
+#     action = model.predict(state)   # 假设有一个训练好的模型， 能够通过当前状态预测出这时应该进行的动作
+#     next_state, reward, done, info = env.step(action)  # 让环境执行动作，获得执行动作的下一个状态，动作的状态，游戏是否结束以及额外信息
+#     if done:                        # 游戏结束则退出循环
+#         break
 
 # 我们的任务就是训练出一个模型，能够根据当前的状态预测出应该进行的一个好的动作。粗略地说，一个好的动作应当能够最大化整个游戏过程中获得的奖励之和，
 # 这也是强化学习的目标。以 CartPole 游戏为例，我们的目标是希望做出合适的动作使得杆一直不倒，即游戏交互的回合数尽可能地多。而回合每进行一次，
@@ -61,8 +61,10 @@ class QNetwork(tf.keras.Model):
         q_values = self(inputs)
         return tf.argmax(q_values, axis=-1)
 
+
 # Q learning 算法
-if __name__ = '__main__':
+
+if __name__ == '__main__':
     env = gym.make('CartPole-v1')  # 实例化一个游戏环境，参数为游戏名称
     model = QNetwork()
     optimizer = tf.keras.optimizers.Adam(learning_rate=learning_rate)
@@ -77,5 +79,37 @@ if __name__ = '__main__':
             env.render()  # 对当前帧进行渲染， 绘图到屏幕
             if random.random() < epsilon:  # epsilon-greedy 探索策略，以epsilon的概率选择随机动作
                 action = env.action_space.sample()
+            else:
+                action = model.predict(np.expand_dims(state, axis=0)).numpy()  # 选择模型计算出的 Q Value 最大的动作
+                action = action[0]
 
+                # 让环境执行动作，获得执行完动作的下一个状态，动作的奖励，游戏是否已结束以及额外信息
+            next_state, reward, done, info = env.step(action)
+            # 如果游戏Game Over，给予大的负奖励
+            reward = -10. if done else reward
+            # 将(state, action, reward, next_state)的四元组（外加 done 标签表示是否结束）放入经验回放池
+            replay_buffer.append((state, action, reward, next_state, 1 if done else 0))
+            # 更新当前 state
+            state = next_state
 
+            if done:  # 游戏结束则退出本轮循环，进行下一个 episode
+                print("episode %4d, epsilon %.4f, score %4d" % (episode_id, epsilon, t))
+                break
+
+            if len(replay_buffer) >= batch_size:
+                # 从经验回放池中随机取一个批次的四元组，并分别转换为 NumPy 数组
+                batch_state, batch_action, batch_reward, batch_next_state, batch_done = \
+                    map(np.array, zip(*random.sample(replay_buffer, batch_size)))
+
+                q_value = model(batch_next_state)
+                y = batch_reward + (gamma * tf.reduce_max(q_value, axis=1)) * (1 - batch_done)  # 计算 y 值
+                with tf.GradientTape() as tape:
+                    loss = tf.keras.losses.mean_squared_error(  # 最小化 y 和 Q-value 的距离
+                        y_true=y,
+                        y_pred=tf.reduce_sum(model(batch_state) * tf.one_hot(batch_action, depth=2), axis=1)
+                    )
+                grads = tape.gradient(loss, model.variables)
+                optimizer.apply_gradients(grads_and_vars=zip(grads, model.variables))  # 计算梯度并更新参数
+
+# 而考虑到需要从图像信息中提取特征，使用 CNN 作为拟合 Q 函数的网络将更为适合。由此，将上面的 QNetwork 更换为 CNN 网络，并对状态做一些修改，
+# 即可用于玩一些简单的视频游戏。
